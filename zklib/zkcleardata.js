ce42b95ed4239bd3d80436548aaef99ecec30b08
ahmad safiul ubab <safiul@syafi>
2016-10-04 22:11:25 +0700

    change clear data

--- zklib/zkcleardata.js
+++ zklib/zkcleardata.js
@@ -3,6 +3,6 @@ var dgram = require('dgram');
 module.exports = function(ZKLib) {
 
 	ZKLib.prototype.cleardata = function (cb) {
-		return this._executeCmd( this.CMD_CLEAR_DATA, '', cb );
+		return this._executeCmd( this.CMD_ATTLOG_RRQ, '', cb );
 	}
 }
